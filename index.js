121814618aa162f23be1a3c137b4b88a2116f227
Logeshwaran K <klogeshwaran003@gmail.com>
2023-09-10 20:07:00 +0530

    docs added

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+/**
+ * Represents a client for interacting with the ToyDB API.
+ * @class
+ */
 class ToyDbQuery {
+  /**
+   * Create a new instance of ToyDbQuery with the specified API URL.
+   * @constructor
+   * @param {string} url - The URL of the ToyDB API.
+   */
   constructor(url) {
     this.url = new URL(url);
     this.urlString = this.url.toJSON();
   }
+
+  /**
+   * Retrieves a list of all collections in the database.
+   * @method
+   * @returns {Promise} A promise that resolves to an array of collection names.
+   */
 
   listCollection() {
     let u = this.urlString;
@@ -22,6 +37,14 @@ class ToyDbQuery {
     });
     return p;
   }
+
+  /**
+   * Creates a new collection with the specified name.
+   * @method
+   * @param {string} collectionName - The name of the new collection.
+   * @returns {Promise} A promise that resolves to a success message.
+   */
+
   createColllection(collectionName) {
     let u = this.urlString + "collection?";
     let params = new URLSearchParams();
@@ -42,6 +65,15 @@ class ToyDbQuery {
     });
     return p;
   }
+
+  /**
+   * Adds a new record to the specified collection.
+   * @method
+   * @param {string} collectionName - The name of the collection to add the record to.
+   * @param {object} data - An object containing the data to be added as a new record.
+   * @returns {Promise} A promise that resolves to a success message.
+   */
+
   addRecord(collectionName, data) {
     let u = this.urlString + "records?";
     let params = new URLSearchParams();
@@ -67,6 +99,15 @@ class ToyDbQuery {
     });
     return p;
   }
+
+  /**
+   * Queries records from a collection with optional limiting.
+   * @method
+   * @param {Object} options - The query options.
+   * @param {string} options.collection - The name of the collection to query.
+   * @param {number} [options.limit=10] - The maximum number of records to retrieve (default: 10).
+   * @returns {Promise} A promise that resolves to an array of queried records.
+   */
 
   query({ collection, limit }) {
     if (!collection) {
@@ -94,6 +135,15 @@ class ToyDbQuery {
     });
     return p;
   }
+
+  /**
+   * Finds a record in a collection by its ID.
+   * @method
+   * @param {Object} options - The find options.
+   * @param {string} options.collection - The name of the collection to search.
+   * @param {string} options.id - The ID of the record to find.
+   * @returns {Promise} A promise that resolves to the found record.
+   */
 
   find({ collection, id }) {
     if (!collection || !id) {
@@ -124,6 +174,16 @@ class ToyDbQuery {
     });
     return p;
   }
+
+  /**
+   * Queries records from a collection where a specific field matches a value.
+   * @method
+   * @param {Object} options - The query options.
+   * @param {string} options.collection - The name of the collection to query.
+   * @param {string} options.field - The field to match.
+   * @param {any} options.value - The value to match against the specified field.
+   * @returns {Promise} A promise that resolves to an array of matching records.
+   */
   where({ field, value, collection }) {
     if (!collection || !field || !value) {
       let p = new Promise((resolve, reject) => {
@@ -155,7 +215,16 @@ class ToyDbQuery {
     return p;
   }
 
-  
+  /**
+   * Updates a record in a collection with a new value for a specific field.
+   * @method
+   * @param {Object} options - The update options.
+   * @param {string} options.collection - The name of the collection to update.
+   * @param {string} options.field - The field to update.
+   * @param {any} options.value - The new value to set for the specified field.
+   * @param {string} options.id - The ID of the record to update.
+   * @returns {Promise} A promise that resolves to a success message.
+   */
   updateRecord({ field, value, collection, id }) {
     if (!collection || !field || !value || !id) {
       let p = new Promise((resolve, reject) => {
@@ -189,6 +258,16 @@ class ToyDbQuery {
     return p;
   }
 
+  /**
+   * Adds a new field with a value to a record in a collection.
+   * @method
+   * @param {Object} options - The add field options.
+   * @param {string} options.collection - The name of the collection to update.
+   * @param {string} options.field - The new field to add.
+   * @param {any} options.value - The value to set for the new field.
+   * @param {string} options.id - The ID of the record to update.
+   * @returns {Promise} A promise that resolves to a success message.
+   */
   addNewField({ field, value, collection, id }) {
     if (!collection || !field || !value || !id) {
       let p = new Promise((resolve, reject) => {
